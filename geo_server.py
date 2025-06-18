cdb7c57aa671cd49a5141a614ed20b3d81f04746
barakhtyanskiyyy <barakhtyanskiy.86@gmail.com>
2025-06-18 07:04:21 +0500

    Update geo_server.py

--- geo_server.py
+++ geo_server.py
@@ -1,25 +1,20 @@
 from flask import Flask, request
 import requests
-import os
 
 app = Flask(__name__)
-IPINFO_TOKEN = os.getenv("4e9a9682e56c8b")
+
+IPINFO_TOKEN = "4e9a9682e56c8b"
 
 @app.route('/')
 def get_geo():
     user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+   response = requests.get(f"https://ipinfo.io/{user_ip}?token={IPINFO_TOKEN}").json()
 
-    if not IPINFO_TOKEN:
-        return "Ошибка: токен IPinfo не указан."
-
-    response = requests.get(f"https://ipinfo.io/{user_ip}?token={IPINFO_TOKEN}")
-    data = response.json()
-
-    city = data.get('city', 'Нет данных')
-    region = data.get('region', 'Нет данных')
-    country = data.get('country', 'Нет данных')
-    org = data.get('org', 'Нет данных')
-    loc = data.get('loc', 'Нет данных')
+    city = response.get("city")
+    region = response.get("region")
+    country = response.get("country")
+    org = response.get("org")
+    loc = response.get("loc")
 
     return f"""
     <h1>IP: {user_ip}</h1>
@@ -30,6 +25,5 @@ def get_geo():
     <p>Локация (широта,долгота): {loc}</p>
     """
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)  # порт Render сам подставит через render.yaml
